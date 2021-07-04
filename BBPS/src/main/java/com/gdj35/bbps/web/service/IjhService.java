e20bb1273f3a0673be8042663426efa8bdbc7bbb
권재현 <@DESKTOP-ALKPIF9>
2021-07-04 20:47:54 +0900

    본사로그인구현...하지만 미완성

---
+++ BBPS/src/main/java/com/gdj35/bbps/web/service/IjhService.java
@@ -0,0 +1,9 @@
+package com.gdj35.bbps.web.service;
+
+import java.util.HashMap;
+
+public interface IjhService {
+
+public HashMap<String, String> getHQ(HashMap<String, String> params) throws Throwable;
+
+}
