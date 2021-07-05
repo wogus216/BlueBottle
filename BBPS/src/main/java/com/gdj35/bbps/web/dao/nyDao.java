eb8329a0bf47a95a0a9237b7c1f0e7efba0d0208
고나연 <suda619@naver.com>
2021-07-05 17:03:59 +0900

    인터페이스 수정

--- BBPS/src/main/java/com/gdj35/bbps/web/dao/nyDao.java
+++ BBPS/src/main/java/com/gdj35/bbps/web/dao/nyDao.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class nyDao implements INyDao{
+public class nyDao implements InyDao{
 
 	@Autowired
 	public SqlSession sqlSession;
--- BBPS/src/main/java/com/gdj35/bbps/web/service/nyService.java
+++ BBPS/src/main/java/com/gdj35/bbps/web/service/nyService.java
@@ -3,11 +3,11 @@ package com.gdj35.bbps.web.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.gdj35.bbps.web.dao.INyDao;
+import com.gdj35.bbps.web.dao.InyDao;
 
 @Service
-public class nyService implements INyService{
+public class nyService implements InyService{
 
 	@Autowired
-	public INyDao iNyDao;
+	public InyDao iNyDao;
 }
