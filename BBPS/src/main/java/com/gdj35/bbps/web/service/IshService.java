548500a42940270f80e89348b07e549139dadad6
김시훈 <sua209@naver.com>
2021-07-05 17:20:28 +0900

    본사 주문조회 리스트 수정중

--- BBPS/src/main/java/com/gdj35/bbps/web/service/IshService.java
+++ BBPS/src/main/java/com/gdj35/bbps/web/service/IshService.java
@@ -1,5 +1,11 @@
 package com.gdj35.bbps.web.service;
 
+import java.util.HashMap;
+import java.util.List;
+
 public interface IshService {
+	public int getOCnt(HashMap<String, String> params) throws Throwable;
+
+	public List<HashMap<String, String>> getOList(HashMap<String, String> params) throws Throwable;
 
 }
--- BBPS/src/main/java/com/gdj35/bbps/web/service/shService.java
+++ BBPS/src/main/java/com/gdj35/bbps/web/service/shService.java
@@ -1,5 +1,8 @@
 package com.gdj35.bbps.web.service;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +12,14 @@ import com.gdj35.bbps.web.dao.IshDao;
 public class shService implements IshService{
 	@Autowired
 	public IshDao ishDao;
+
+	@Override
+	public int getOCnt(HashMap<String, String> params) throws Throwable {
+		return ishDao.getOCnt(params);
+	}
+
+	@Override
+	public List<HashMap<String, String>> getOList(HashMap<String, String> params) throws Throwable {
+		return ishDao.getOList(params);
+	}
 }
