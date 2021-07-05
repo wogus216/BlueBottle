5064a4c37fea5669dfb0cf80cb2e0daf003b518e
정슬기 <sense0626@naver.com>
2021-07-05 13:43:17 +0900

    ss

--- BBPS/src/main/java/com/gdj35/bbps/web/controller/sgController.java
+++ BBPS/src/main/java/com/gdj35/bbps/web/controller/sgController.java
@@ -1,8 +1,18 @@
 package com.gdj35.bbps.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.gdj35.bbps.common.service.IPagingService;
+import com.gdj35.bbps.web.service.IsgService;
 
 @Controller
 public class sgController {
+	@Autowired
+	public IsgService isgService;
+	@Autowired
+	public IPagingService ipagingService;
+	
+	
 
 }
