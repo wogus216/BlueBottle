f694cde7c7c0d6e6ffc8099fb355688f53ba79a2
고나연 <suda619@naver.com>
2021-07-05 17:07:59 +0900

    컨트롤러 수정

--- BBPS/src/main/java/com/gdj35/bbps/web/controller/nyController.java
+++ BBPS/src/main/java/com/gdj35/bbps/web/controller/nyController.java
@@ -4,13 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.gdj35.bbps.common.service.IPagingService;
-import com.gdj35.bbps.web.service.INyService;
+import com.gdj35.bbps.web.service.InyService;
 
 @Controller
 public class nyController {
 
 	@Autowired
-	public INyService iNyService;
+	public InyService iNyService;
 	
 	@Autowired
 	public IPagingService iPagingService;
