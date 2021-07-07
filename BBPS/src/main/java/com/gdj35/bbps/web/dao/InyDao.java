60a3b6b617f37ed4f6f42708adfd1e3603bd823e
최정민 <dndbejtk@naver.com>
2021-07-07 17:39:29 +0900

    나연 7/7

--- BBPS/src/main/java/com/gdj35/bbps/web/dao/InyDao.java
+++ BBPS/src/main/java/com/gdj35/bbps/web/dao/InyDao.java
@@ -1,5 +1,14 @@
 package com.gdj35.bbps.web.dao;
 
+import java.util.HashMap;
+import java.util.List;
+
 public interface InyDao {
+
+	List<HashMap<String, String>> getCateList() throws Throwable;
+
+	int addCate(HashMap<String, String> params) throws Throwable;
+
+	int updateCate(HashMap<String, String> params) throws Throwable;
 
 }
--- BBPS/src/main/java/com/gdj35/bbps/web/service/nyService.java
+++ BBPS/src/main/java/com/gdj35/bbps/web/service/nyService.java
@@ -1,5 +1,8 @@
 package com.gdj35.bbps.web.service;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +13,24 @@ public class nyService implements InyService{
 
 	@Autowired
 	public InyDao iNyDao;
+
+	@Override
+	public List<HashMap<String, String>> getCateList() throws Throwable {
+
+		return iNyDao.getCateList();
+	}
+
+	@Override
+	public int addCate(HashMap<String, String> params) throws Throwable {
+
+		return iNyDao.addCate(params);
+	}
+
+	@Override
+	public int updateCate(HashMap<String, String> params) throws Throwable {
+
+		return iNyDao.updateCate(params);
+	}
+
+
 }
